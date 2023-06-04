fc58b4b830906e75c731ec9293c90d79949da77a
nikitakuptsov <kuptsovns@gmail.com>
2023-06-05 01:30:11 +0300

    фикс файла README.md

--- src/main/java/ru/job4j/shortcut/ShortCutApp.java
+++ src/main/java/ru/job4j/shortcut/ShortCutApp.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class ShortCutApp {
     public static void main(String[] args) {
         SpringApplication.run(ShortCutApp.class, args);
     }
-
-
 }
